cb0c6b36dbc0d790de06f5358487fd64b36ad250
Evgeniy <lysenko765@yandex.ru>
2020-03-31 20:15:20 +0300

    Переход к страницам людей осуществляется через адрес, а не по ключу

--- src/main/java/pstgu/NmMap/web/SimpleController.java
+++ src/main/java/pstgu/NmMap/web/SimpleController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pstgu.NmMap.application.MtSimpleStorage;
@@ -17,7 +18,7 @@ public class SimpleController {
 
 	MtSimpleStorage storage = new MtSimpleStorage("resources/output");
 
-	@GetMapping("/list")
+	@GetMapping("/persons")
 	public String listPage(Model model) {
 		List<Human> persons = new ArrayList<Human>(storage.getMap().values());
 		model.addAttribute("persons", persons);
@@ -25,8 +26,8 @@ public class SimpleController {
 		return "list";
 	}
 
-	@GetMapping("/human")
-	public String personPage(@RequestParam("id") int id, Model model) {
+	@GetMapping("/persons/{id}")
+	public String personPage(@PathVariable("id") int id, Model model) {
 		Human human = storage.getHuman(id);
 		model.addAttribute("human", human);
 
